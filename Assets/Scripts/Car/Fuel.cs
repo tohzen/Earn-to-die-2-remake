d137623db73d7e9ee065f44e387672e0f972aa64
AlexGub4454 <56447911+AlexGub4454@users.noreply.github.com>
2020-09-17 12:16:19 +0300

    All works coomit

---
+++ Assets/Scripts/Car/Fuel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fuel : MonoBehaviour
+{
+    [SerializeField]
+    private float _timeToEndFuel = 20f;
+    private CarController _car;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        _timeToEndFuel -= Time.deltaTime;
+        if (_timeToEndFuel < 0)
+        {
+         //   _car.SetCarForce(0);
+        }
+    }
+}
---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+[RequireComponent(typeof(Rigidbody2D))]
+public class Obstacle : MonoBehaviour
+{
+    [SerializeField]
+    private float _timeToDestroy=1f;
+    [SerializeField]
+    private float _obstacleHardness = 10f;
+    [SerializeField]
+    private Animation _anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag != "Player") { return; }
+        if (collision.relativeVelocity.magnitude > 10)
+        {
+            //Play anim
+            Destroy(gameObject);
+         
+        }
+        else
+        {
+            StartCoroutine(DestroyObstacle());
+        }
+    }
+   
+    private IEnumerator DestroyObstacle()
+    {
+        yield return new WaitForSeconds(_timeToDestroy);
+        Destroy(gameObject);
+    }
+}
